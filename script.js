b9b952059b99a5214b1e9ef38f158e2bb61c9836
orlandojpwastaken <144786542+orlandojpwastaken@users.noreply.github.com>
2023-10-31 10:40:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,89 @@ function onClick() {
   }
 }
 
-function clickCircle() {
-  const buttons = document.querySelectorAll(".circular-button");
-  buttons.forEach(button => {
-    button.addEventListener("click", function () {
-        this.classList.toggle("clicked");
-    });
-})
-}
-
 function logOut() {
- var leave =  confirm("Are you sure you want to log out?")
- if(leave == true){
-  location.assign("index.html")
+  var leave =  confirm("Are you sure you want to log out?")
+  if(leave == true){
+    location.assign("index.html")
+  }
  }
- 
- }
+
+  function buttonPress1() {
+    var x = document.getElementById("AppButton1");
+    if (x.className === "circular-button1") {
+      x.className += " active";
+    } else {
+      x.className = "circular-button1";
+    }
+  }
+  function buttonPress2() {
+    var x = document.getElementById("AppButton2");
+    if (x.className === "circular-button2") {
+      x.className += " active";
+    } else {
+      x.className = "circular-button2";
+    }
+  }
+
+  function buttonPress3() {
+    var x = document.getElementById("AppButton3");
+    if (x.className === "circular-button3") {
+      x.className += " active";
+    } else {
+      x.className = "circular-button3";
+    }
+  }
+
+  function buttonPress4() {
+    var x = document.getElementById("AppButton4");
+    if (x.className === "circular-button4") {
+      x.className += " active";
+    } else {
+      x.className = "circular-button4";
+    }
+  }
+
+  function buttonPress5() {
+    var x = document.getElementById("AppButton5");
+    if (x.className === "circular-button5") {
+      x.className += " active";
+    } else {
+      x.className = "circular-button5";
+    }
+  }
+
+  function buttonPress6() {
+    var x = document.getElementById("AppButton6");
+    if (x.className === "circular-button6") {
+      x.className += " active";
+    } else {
+      x.className = "circular-button6";
+    }
+  }
+
+  function buttonPress7() {
+    var x = document.getElementById("AppButton7");
+    if (x.className === "circular-button7") {
+      x.className += " active";
+    } else {
+      x.className = "circular-button7";
+    }
+  }
+
+  function buttonPress8() {
+    var x = document.getElementById("AppButton8");
+    if (x.className === "circular-button8") {
+      x.className += " active";
+    } else {
+      x.className = "circular-button8";
+    }
+  }
+
+  function buttonPress9() {
+    var x = document.getElementById("AppButton9");
+    if (x.className === "circular-button9") {
+      x.className += " active";
+    } else {
+      x.className = "circular-button9";
+    }
+  }
